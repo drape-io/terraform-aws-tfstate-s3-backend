f19bd8f72795307c97fb058820f1cee010670166
John Anderson <sontek@gmail.com>
2023-12-26 18:46:31 -0400

    use dyamo for locking

--- examples/remote/config.tf
+++ examples/remote/config.tf
@@ -19,15 +19,16 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "drape-customer1-dev-k8s-tfstate"
-    key    = "app1"
-    region = "us-east-1"
-    endpoint= "http://localhost:4566"
+    bucket                      = "drape-customer1-dev-k8s-tfstate"
+    key                         = "app1"
+    region                      = "us-east-1"
+    endpoint                    = "http://localhost:4566"
+    dynamodb_table              = "drape-customer1-dev-k8s-tfstate"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
 
-    force_path_style            = true
-    dynamodb_endpoint           = "http://localhost:4566"
+    force_path_style  = true
+    dynamodb_endpoint = "http://localhost:4566"
   }
 }
